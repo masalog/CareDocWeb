45b222aec4489fe024d88175edb50c67641bdd31
masalog <masalog@example.com>
2026-06-29 20:10:11 +0900

    feat : CareDocWeb ApplicationTests が、IntelliJの全プロジェクトをロードしないように、@ActiveProfiles を指定。ローカルPCのシステム環境変数が MySQL を読み込まないように、@TestPropertySource にてH2データベースを指定

--- src/test/java/com/example/CareDocWeb/CareDocWebApplicationTests.java
+++ src/test/java/com/example/CareDocWeb/CareDocWebApplicationTests.java
@@ -2,12 +2,22 @@ package com.example.CareDocWeb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("local")
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:caredoc-test",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class CareDocWebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
